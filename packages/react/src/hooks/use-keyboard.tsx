f90f6fbf26aaa19fb6711ce0a4ae95013ddaffd4
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-08-31 23:37:26 +0200

    More efficient `useKeyboard` (#118)
    I added useEvent which let you create a stable function that always uses
    the latest version
    
    This way there is no need to pass handler to the useEffect dependencies
    (which would usually be different on every render)

--- packages/react/src/hooks/use-keyboard.tsx
+++ packages/react/src/hooks/use-keyboard.tsx
@@ -1,15 +1,17 @@
 import type { ParsedKey } from "@opentui/core"
 import { useEffect } from "react"
 import { useAppContext } from "../components/app"
+import { useEvent } from "./use-event"
 
 export const useKeyboard = (handler: (key: ParsedKey) => void) => {
   const { keyHandler } = useAppContext()
+  const stableHandler = useEvent(handler)
 
   useEffect(() => {
-    keyHandler?.on("keypress", handler)
+    keyHandler?.on("keypress", stableHandler)
 
     return () => {
-      keyHandler?.off("keypress", handler)
+      keyHandler?.off("keypress", stableHandler)
     }
-  }, [keyHandler, handler])
+  }, [keyHandler, stableHandler])
 }
