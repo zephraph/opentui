84258218e4825ba1ae3bf523013e603a7e51feab
reversegem_7 <reversegem7@gmail.com>
2025-08-31 21:12:38 +0200

    chore: remove unused code and run prettier

--- packages/core/src/examples/scroll-area.ts
+++ packages/core/src/examples/scroll-area.ts
@@ -10,55 +10,55 @@ let box2: BoxRenderable | null = null
 let box3: BoxRenderable | null = null
 
 export function run(rendererInstance: CliRenderer): void {
-    renderer = rendererInstance
-    renderer.setBackgroundColor("#001122")
+  renderer = rendererInstance
+  renderer.setBackgroundColor("#001122")
 
-    root = new ScrollAreaRenderable(renderer, {
-        height: 20,
-        width: 20,
-    })
+  root = new ScrollAreaRenderable(renderer, {
+    height: 20,
+    width: 20,
+  })
 
-    box = new BoxRenderable(renderer, {
-        width: 1000,
-        height: 10,
-        backgroundColor: "#f00"
-    })
+  box = new BoxRenderable(renderer, {
+    width: 1000,
+    height: 10,
+    backgroundColor: "#f00",
+  })
 
-    box2 = new BoxRenderable(renderer, {
-        width: 1000,
-        height: 10,
-        backgroundColor: "#ff0"
-    })
+  box2 = new BoxRenderable(renderer, {
+    width: 1000,
+    height: 10,
+    backgroundColor: "#ff0",
+  })
 
-    box3 = new BoxRenderable(renderer, {
-        width: 1000,
-        height: 10,
-        backgroundColor: "#f0f"
-    })
+  box3 = new BoxRenderable(renderer, {
+    width: 1000,
+    height: 10,
+    backgroundColor: "#f0f",
+  })
 
-    renderer.root.add(root)
-    root.add(box)
-    root.add(box2)
-    root.add(box3)
-    root.focus()
+  renderer.root.add(root)
+  root.add(box)
+  root.add(box2)
+  root.add(box3)
+  root.focus()
 }
 
 export function destroy(rendererInstance: CliRenderer): void {
-    if (root) {
-        rendererInstance.root.remove(root.id)
-        root.destroy()
-        root = null
-    }
+  if (root) {
+    rendererInstance.root.remove(root.id)
+    root.destroy()
+    root = null
+  }
 
-    renderer = null
+  renderer = null
 }
 
 if (import.meta.main) {
-    const renderer = await createCliRenderer({
-        exitOnCtrlC: true,
-    })
+  const renderer = await createCliRenderer({
+    exitOnCtrlC: true,
+  })
 
-    run(renderer)
-    setupCommonDemoKeys(renderer)
-    renderer.start()
+  run(renderer)
+  setupCommonDemoKeys(renderer)
+  renderer.start()
 }
--- packages/core/src/renderables/ScrollArea.ts
+++ packages/core/src/renderables/ScrollArea.ts
@@ -1,260 +1,257 @@
 import { Edge } from "yoga-layout"
-import { MouseEvent, OptimizedBuffer, parseColor, Renderable, RGBA, type ParsedKey, type RenderableOptions, type RenderContext } from ".."
+import {
+  MouseEvent,
+  OptimizedBuffer,
+  parseColor,
+  Renderable,
+  RGBA,
+  type ParsedKey,
+  type RenderableOptions,
+  type RenderContext,
+} from ".."
 
 export type Size = {
-    width: number,
-    height: number
+  width: number
+  height: number
 }
 
 function clamp(v: number, min: number, max: number) {
-    return Math.min(Math.max(v, min), max)
-}
-
-function isVertical(orientation: "horizontal" | "vertical"): orientation is "vertical" {
-    return orientation === "vertical"
+  return Math.min(Math.max(v, min), max)
 }
 
 class ViewportRenderable extends Renderable {
-    protected focusable: boolean = true
+  protected focusable: boolean = true
 
-    constructor(ctx: RenderContext, options: RenderableOptions) {
-        super(ctx, options)
-    }
+  constructor(ctx: RenderContext, options: RenderableOptions) {
+    super(ctx, options)
+  }
 
-    public get size(): Size {
-        const node = this.getLayoutNode().yogaNode
-        return {
-            width: node.getComputedWidth(),
-            height: node.getComputedHeight()
-        }
+  public get size(): Size {
+    const node = this.getLayoutNode().yogaNode
+    const { width, height } = node.getComputedLayout()
+    return {
+      width,
+      height,
     }
+  }
 }
 
 export class ScrollBarRenderable extends Renderable {
-    private _contentSize: Size = {
-        width: 0,
-        height: 0
-    }
+  private _contentSize: Size = {
+    width: 0,
+    height: 0,
+  }
 
-    private _viewportSize: Size = {
-        width: 0,
-        height: 0
-    }
+  private _viewportSize: Size = {
+    width: 0,
+    height: 0,
+  }
 
-    private _trackColor: RGBA
-    private _thumbColor: RGBA
+  private _trackColor: RGBA
+  private _thumbColor: RGBA
 
-    private _thumbPos = 0
-    private _thumbSize = 0
+  private _thumbPos = 0
+  private _thumbSize = 0
 
-    private _defaultOptions = {
-        trackColor: RGBA.fromValues(0.15, 0.15, 0.16),
-        thumbColor: RGBA.fromValues(0.62, 0.62, 0.66),
-        orientation: "vertical"
-    }
+  private _defaultOptions = {
+    trackColor: RGBA.fromValues(0.15, 0.15, 0.16),
+    thumbColor: RGBA.fromValues(0.62, 0.62, 0.66),
+    orientation: "vertical",
+  }
 
-    constructor(ctx: RenderContext, options: RenderableOptions) {
-        super(ctx, options)
+  constructor(ctx: RenderContext, options: RenderableOptions) {
+    super(ctx, options)
 
-        this._trackColor = parseColor(this._defaultOptions.trackColor)
-        this._thumbColor = parseColor(this._defaultOptions.thumbColor)
-    }
+    this._trackColor = parseColor(this._defaultOptions.trackColor)
+    this._thumbColor = parseColor(this._defaultOptions.thumbColor)
+  }
 
-    public get thumbPos() {
-        return this._thumbPos
-    }
+  public get thumbPos() {
+    return this._thumbPos
+  }
 
-    public set thumbPos(pos: number) {
-        this._thumbPos = pos
-        this.needsUpdate()
-    }
+  public set thumbPos(pos: number) {
+    this._thumbPos = pos
+    this.needsUpdate()
+  }
 
-    public get thumbSize() {
-        return this._thumbSize
-    }
+  public get thumbSize() {
+    return this._thumbSize
+  }
 
-    public set thumbSize(size: number) {
-        this._thumbSize = size
-        this.needsUpdate()
-    }
+  public set thumbSize(size: number) {
+    this._thumbSize = size
+    this.needsUpdate()
+  }
 
-    public get contentSize() {
-        return this._contentSize
-    }
+  public get contentSize() {
+    return this._contentSize
+  }
 
-    public set contentSize(size: Size) {
-        this._contentSize = size
-        this.needsUpdate()
-    }
+  public set contentSize(size: Size) {
+    this._contentSize = size
+    this.needsUpdate()
+  }
 
-    public get viewportSize() {
-        return this._viewportSize
-    }
+  public get viewportSize() {
+    return this._viewportSize
+  }
 
-    public set viewportSize(size: Size) {
-        this._viewportSize = size
-        this.needsUpdate()
-    }
+  public set viewportSize(size: Size) {
+    this._viewportSize = size
+    this.needsUpdate()
+  }
 
-    protected onMouseEvent(event: MouseEvent): void {
-    }
+  protected onMouseEvent(event: MouseEvent): void {}
 
-    protected renderSelf(buffer: OptimizedBuffer, deltaTime: number): void {
-        buffer.fillRect(this.x, this.y, this.width, this._viewportSize.height, this._trackColor)
+  protected renderSelf(buffer: OptimizedBuffer, deltaTime: number): void {
+    buffer.fillRect(this.x, this.y, this.width, this._viewportSize.height, this._trackColor)
 
-        buffer.fillRect(this.x, this.thumbPos, this.width, this.thumbSize, this._thumbColor)
+    buffer.fillRect(this.x, this.thumbPos, this.width, this.thumbSize, this._thumbColor)
 
-        console.log(this._viewportSize, this._contentSize);
-    }
+    console.log(this._viewportSize, this._contentSize)
+  }
 }
 
 type ScrollBarOptions = RenderableOptions & {
-    thickness?: number
+  thickness?: number
 }
 
 type ScrollAreaOptions = RenderableOptions & {
-    scrollbars?: {
-        vertical: ScrollBarOptions
-        horizontal: ScrollBarOptions
-    }
+  scrollbars?: {
+    vertical: ScrollBarOptions
+    horizontal: ScrollBarOptions
+  }
 }
 
 export class ScrollAreaRenderable extends Renderable {
-    protected focusable: boolean = true
+  protected focusable: boolean = true
 
-    protected viewport: ViewportRenderable
-    protected vertical: ScrollBarRenderable;
+  protected viewport: ViewportRenderable
+  protected vertical: ScrollBarRenderable
 
-    protected _contentSize: Size = {
-        width: 0,
-        height: 0
+  protected _contentSize: Size = {
+    width: 0,
+    height: 0,
+  }
+  protected _viewportSize: Size = {
+    width: 0,
+    height: 0,
+  }
+
+  private _offset: number = 0
+  private _maxOffset: number = 0
+  private _ratio: number = 0
+
+  protected verticalThickness = 2
+  protected horizontalThickness = 1
+
+  constructor(ctx: RenderContext, options: ScrollAreaOptions) {
+    super(ctx, { ...options, buffered: true })
+
+    this.viewport = new ViewportRenderable(ctx, {
+      onKeyDown: (key) => this.onKeyPress(key),
+    })
+
+    this.vertical = new ScrollBarRenderable(ctx, {
+      position: "absolute",
+      top: 0,
+      right: 0,
+      width: this.verticalThickness,
+    })
+
+    super.add(this.viewport)
+    super.add(this.vertical)
+    this.setChildHostById(this.viewport.id)
+
+    this.viewport.focus()
+  }
+
+  private move(delta: number) {
+    const newOffset = clamp(this._offset + delta, 0, this._maxOffset)
+    const ratio = this._maxOffset ? newOffset / this._maxOffset : 0
+
+    this._ratio = ratio
+    this.offset = newOffset
+  }
+
+  private set offset(newOffset: number) {
+    if (newOffset !== this._offset) {
+      this._offset = newOffset
+      this.updateThumb()
+
+      this.viewport.getLayoutNode().yogaNode.setPosition(Edge.Top, -this._offset)
     }
-    protected _viewportSize: Size = {
-        width: 0,
-        height: 0
+  }
+
+  private updateMaxOffset() {
+    const maxOffset = Math.max(this._contentSize.height - this.height, 0)
+
+    if (maxOffset !== this._maxOffset) {
+      this._maxOffset = maxOffset
     }
+  }
 
-    private _offset: number = 0
-    private _maxOffset: number = 0
-    private _ratio: number = 0
+  private updateThumb() {
+    const size = this._viewportSize.height
+    const content = this._contentSize.height
 
-    protected verticalThickness = 2
-    protected horizontalThickness = 1
+    const thumbSize = Math.max(1, Math.ceil((size / content) * size))
 
-    constructor(ctx: RenderContext, options: ScrollAreaOptions) {
-        super(ctx, { ...options, buffered: true })
+    const pos = clamp((this._maxOffset ? this._offset / this._maxOffset : 0) * (size - thumbSize), 0, size - thumbSize)
 
-        this.viewport = new ViewportRenderable(ctx, {
-            onKeyDown: (key) => this.onKeyPress(key)
-        })
+    this.vertical.thumbPos = pos
+    this.vertical.thumbSize = thumbSize
+  }
 
-        this.vertical = new ScrollBarRenderable(ctx, {
-            position: "absolute",
-            top: 0,
-            right: 0,
-            width: this.verticalThickness,
-        })
-
-        super.add(this.viewport)
-        super.add(this.vertical)
-        this.setChildHostById(this.viewport.id)
-
-        this.viewport.focus()
+  private updateContentSize() {
+    const newSize = this.viewport.size
+    if (!Bun.deepMatch(newSize, this._contentSize)) {
+      this._contentSize = newSize
+      this.vertical.contentSize = newSize
     }
+  }
 
-    private move(delta: number) {
-        const newOffset = clamp(this._offset + delta, 0, this._maxOffset)
-        const ratio = this._maxOffset ? newOffset / this._maxOffset : 0
-
-        this._ratio = ratio
-        this.offset = newOffset;
-    }
-
-    private set offset(newOffset: number) {
-        if (newOffset !== this._offset) {
-            this._offset = newOffset
-            this.updateThumb()
-
-            this.viewport.getLayoutNode().yogaNode.setPosition(
-                Edge.Top,
-                -this._offset
-            )
-        }
-    }
-
-    private updateMaxOffset() {
-        const maxOffset = Math.max(this._contentSize.height - this.height, 0)
-
-        if (maxOffset !== this._maxOffset) {
-            this._maxOffset = maxOffset
-        }
-    }
-
-    private updateThumb() {
-        const size = this._viewportSize.height
-        const content = this._contentSize.height
-
-        const thumbSize = Math.max(1, Math.ceil((size / content) * size))
-
-        const pos = clamp(
-            (this._maxOffset ? this._offset / this._maxOffset : 0) * (size - thumbSize),
-            0,
-            size - thumbSize,
-        )
-
-        this.vertical.thumbPos = pos
-        this.vertical.thumbSize = thumbSize
-    }
-
-    private updateContentSize() {
-        const newSize = this.viewport.size
-        if (!Bun.deepMatch(newSize, this._contentSize)) {
-            this._contentSize = newSize
-            this.vertical.contentSize = newSize
-        }
-    }
-
-    private updateViewportSize() {
-        const node = this.getLayoutNode().yogaNode
-        const newSize: Size = {
-            width: node.getComputedWidth() - this.verticalThickness,
-            height: node.getComputedHeight()
-        }
-
-        if (!Bun.deepMatch(newSize, this._viewportSize)) {
-            this._viewportSize = newSize
-            this.vertical.viewportSize = newSize
-
-            const vertical = this.vertical.getLayoutNode().yogaNode
-            vertical.setHeight(newSize.height - this.horizontalThickness)
-
-        }
+  private updateViewportSize() {
+    const node = this.getLayoutNode().yogaNode
+    const { width, height } = node.getComputedLayout()
+    const newSize: Size = {
+      width: width - this.verticalThickness,
+      height: height,
     }
 
-    protected onResize(): void {
-        this.needsUpdate()
+    if (!Bun.deepMatch(newSize, this._viewportSize)) {
+      this._viewportSize = newSize
+      this.vertical.viewportSize = newSize
+
+      const vertical = this.vertical.getLayoutNode().yogaNode
+      vertical.setHeight(newSize.height - this.horizontalThickness)
+    }
+  }
+
+  protected onResize(): void {
+    this.needsUpdate()
+  }
+
+  public onKeyPress(key: ParsedKey | string): boolean {
+    const keyName = typeof key === "string" ? key : key.name
+
+    switch (keyName) {
+      case "up":
+        this.move(-1)
+        return true
+      case "down":
+        this.move(1)
+        return true
     }
 
-    public onKeyPress(key: ParsedKey | string): boolean {
-        const keyName = typeof key === "string" ? key : key.name
+    return false
+  }
 
-        switch (keyName) {
-            case "up":
-                this.move(-1)
-                return true
-            case "down":
-                this.move(1)
-                return true
-
-        }
-
-        return false
-    }
-
-    protected renderSelf(): void {
-        this.updateViewportSize()
-        this.updateContentSize()
-        this.updateMaxOffset()
-        this.updateThumb()
-    }
+  protected renderSelf(): void {
+    this.updateViewportSize()
+    this.updateContentSize()
+    this.updateMaxOffset()
+    this.updateThumb()
+  }
 }
